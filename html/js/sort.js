401a394fc6f8a37b9817f43ae462bec1d94f7981
denkersdaan-ops <denkersdaan@gmail.com>
2026-03-09 16:31:07 +0100

    fix deslect sorting items

--- html/js/sort.js
+++ html/js/sort.js
@@ -2,7 +2,31 @@ let selectedCategoryId = null;
 
 window.setCategories = function (categorie) {
     selectedCategoryId = categorie;
-    console.log("Selected category:", categorie);
+
+    var allButtons = [];
+    allButtons = document.getElementsByClassName("category");
+
+    var lastButton = document.getElementsByClassName("last-button")[0];
+
+    if (lastButton && lastButton.id == categorie) {
+        lastButton.classList.remove('last-button');
+        lastButton = null;
+        selectedCategoryId = null;
+        console.log("Last button:");
+        filterProducts();
+        return
+    }
+
+    console.log("new button: " + selectedCategoryId);
+
+    for (let i = 0; i < allButtons.length; i++) {
+        allButtons[i].classList.remove('last-button');
+    }
+
+    var categoryButtons = document.getElementById(categorie);
+    categoryButtons.classList.add('last-button');
+
+
     filterProducts();
 }
 
@@ -13,7 +37,7 @@ window.get_idsort = function () {
 window.filterProducts = function () {
     const productList = document.getElementById('product-list');
     const products = productList.querySelectorAll('[id^="categorie:"]');
-    
+
     products.forEach(product => {
         if (selectedCategoryId === null) {
             product.style.display = '';
